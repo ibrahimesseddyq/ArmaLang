4d33a95a980e8298c0c12af15417e6aefbc9fb38
armanovxx <iebeat2018@outlook.com>
2022-05-21 20:51:05 +0200

    add configuration file for arma

---
+++ arma.config.js
@@ -0,0 +1,7 @@
+const armaConfig = {
+    useCustomFilePath: true,
+    bootstrapPath: "example/ex1.darija"
+}
+module.exports = {
+    armaConfig
+}
--- run.js
+++ run.js
@@ -1,12 +1,17 @@
 const fs = require("mz/fs");
 const util = require('util');
+const { config } = require("./arma.config")
 const exec = util.promisify(require('child_process').exec);
 
 async function main() {
     const filename = process.argv[2];
-    if (!filename) {
+    if (!filename && config.useCustomFilePath && !config.bootstrapPath) {
         console.log("Please provide a .darija file.");
         return;
+    } else if (!filename && !config.useCustomFilePath && config.bootstrapPath) {
+        filename = config.bootstrapPath;
+    } else if (!filename && !config.bootstrapPath && config.useCustomFilePath) {
+        throw new Error("Kayn chi problem f configuration")
     }
     const astFilename = filename.replace(".darija", ".ast");
     const jsFilename = filename.replace(".darija", ".js");
